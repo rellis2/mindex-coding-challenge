a7ab83d965f55000f0c911469193c8a9e17e8723
rellis2 <41808175+rellis2@users.noreply.github.com>
2021-04-16 10:35:58 -0400

    Add files via upload

---
+++ mindex-java-code-challenge/src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -0,0 +1,33 @@
+package com.mindex.challenge.data;
+
+
+public class ReportingStructure {
+	private Employee employee;
+	private int numOfReports;
+	
+	//default constructor
+	public ReportingStructure() {
+		
+	}
+	
+	public ReportingStructure(Employee employee, int numOfReports) {
+		this.setEmployee(employee);
+		this.setNumOfReports(numOfReports);
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public int getNumOfReports() {
+		return numOfReports;
+	}
+
+	public void setNumOfReports(int numOfReports) {
+		this.numOfReports = numOfReports;
+	}
+}
